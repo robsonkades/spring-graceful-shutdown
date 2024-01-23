6e361d300537a303f653925d45b6d0d4228dce96
Robson Kades <robsonkades@outlook.com>
2024-01-22 22:49:31 -0300

    initial commit

--- src/main/java/com/robsonkades/springdockerfile/AppConfig.java
+++ src/main/java/com/robsonkades/springdockerfile/AppConfig.java
@@ -1,22 +1,38 @@
 package com.robsonkades.springdockerfile;
 
+import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 public class AppConfig {
 
-    @Autowired
-    Consumer consumer;
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppConfig.class);
 
     @Bean
     ServletListenerRegistrationBean<ServletContextListener> servletListener() {
-        ServletListenerRegistrationBean<ServletContextListener> srb
-                = new ServletListenerRegistrationBean<>();
-        srb.setListener(consumer);
+        ServletListenerRegistrationBean<ServletContextListener> srb = new ServletListenerRegistrationBean<>();
+        srb.setListener(new AwaitServlet());
         return srb;
+    }
+
+    public static class AwaitServlet implements ServletContextListener {
+
+        @Override
+        public void contextDestroyed(ServletContextEvent sce) {
+            LOGGER.info("Inicializando desligamento consumer");
+            try {
+                Thread.sleep(TimeUnit.SECONDS.toMillis(20));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            LOGGER.info("Desligamento completo consumer");
+        }
     }
 }
--- src/main/java/com/robsonkades/springdockerfile/Consumer.java
+++ src/main/java/com/robsonkades/springdockerfile/Consumer.java
@@ -1,62 +1,32 @@
 package com.robsonkades.springdockerfile;
 
-import jakarta.servlet.ServletContextEvent;
-import jakarta.servlet.ServletContextListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.SmartLifecycle;
-import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.TimeUnit;
-
 @Component
-public class Consumer implements  ServletContextListener {
+public class Consumer implements  SmartLifecycle {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Consumer.class);
 
     public boolean isRunning;
-//
-//    @Override
-//    public void start() {
-//        this.isRunning = true;
-//        LOGGER.info("Running start {}", true);
-//    }
-//
-//    @Override
-//    public void stop() {
-//        this.isRunning = false;
-//        LOGGER.info("Running stop {}", false);
-//    }
-//
-//    @Override
-//    public boolean isRunning() {
-//        LOGGER.info("Running isRunning {}", isRunning);
-//        return this.isRunning;
-//    }
-//
-//    @Override
-//    public void onApplicationEvent(ContextClosedEvent event) {
-//        LOGGER.info("onApplicationEvent {}", isRunning);
-//        this.isRunning = false;
-//    }
 
     @Override
-    public void contextInitialized(ServletContextEvent sce) {
+    public void start() {
         this.isRunning = true;
-        LOGGER.info("contextInitialized {}", true);
+        LOGGER.info("Running start {}", true);
     }
 
     @Override
-    public void contextDestroyed(ServletContextEvent sce) {
-        LOGGER.info("Inicializando desligamento consumer");
+    public void stop() {
         this.isRunning = false;
-        try {
-            Thread.sleep(TimeUnit.SECONDS.toMillis(20));
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        LOGGER.info("Desligamento completo consumer");
+        LOGGER.info("Running stop {}", false);
+    }
+
+    @Override
+    public boolean isRunning() {
+        LOGGER.info("Running isRunning {}", isRunning);
+        return this.isRunning;
     }
 }
